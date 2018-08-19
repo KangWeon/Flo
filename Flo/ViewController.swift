f140208775a0fb5efeca8ce5aab691e74d86fec3
KangWeon <kangweon.jeong@gmail.com>
2018-08-19 23:26:05 +0900

    Core Graphics Tutorial Part 2: Gradients and Contexts

--- Flo/ViewController.swift
+++ Flo/ViewController.swift
@@ -13,12 +13,25 @@ class ViewController: UIViewController {
     @IBOutlet weak var counterView: CounterView!
     @IBOutlet weak var counterLabel: UILabel!
     
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var graphView: GraphView!
+    
+    var isGraphViewShowing = false
+    
+    
+    //Label outlets
+    @IBOutlet weak var averageWaterDrunk: UILabel!
+    @IBOutlet weak var maxLabel: UILabel!
+    @IBOutlet weak var stackView: UIStackView!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        counterLabel.text = String(counterView.counter)
+        // counterLabel.text = String(counterView.counter)
+        
     }
-
+    
     @IBAction func pushButtonPressed(_ button: PushButton) {
         if button.isAddButton {
             counterView.counter += 1
@@ -29,7 +42,64 @@ class ViewController: UIViewController {
             }
         }
         counterLabel.text = String(counterView.counter)
+        
+        if isGraphViewShowing {
+            counterViewTap(nil)
+        }
     }
+    
+    
+    @IBAction func counterViewTap(_ gesture: UITapGestureRecognizer?) {
+        if (isGraphViewShowing) {
+            //hide Graph
+            UIView.transition(from: graphView,
+                              to: counterView,
+                              duration: 1.0,
+                              options: [.transitionFlipFromLeft, .showHideTransitionViews],
+                              completion:nil)
+        } else {
+            //show Graph
+            setupGraphDisplay()
+            UIView.transition(from: counterView,
+                              to: graphView,
+                              duration: 1.0,
+                              options: [.transitionFlipFromRight, .showHideTransitionViews],
+                              completion: nil)
+        }
+        isGraphViewShowing = !isGraphViewShowing
+    }
+
+    
+    func setupGraphDisplay() {
+        
+        let maxDayIndex = stackView.arrangedSubviews.count - 1
+        
+        //  1 - replace last day with today's actual data
+        graphView.graphPoints[graphView.graphPoints.count - 1] = counterView.counter
+        //2 - indicate that the graph needs to be redrawn
+        graphView.setNeedsDisplay()
+        maxLabel.text = "\(graphView.graphPoints.max()!)"
+        
+        //  3 - calculate average from graphPoints
+        let average = graphView.graphPoints.reduce(0, +) / graphView.graphPoints.count
+        averageWaterDrunk.text = "\(average)"
+        
+        // 4 - setup date formatter and calendar
+        let today = Date()
+        let calendar = Calendar.current
+        
+        let formatter = DateFormatter()
+        formatter.setLocalizedDateFormatFromTemplate("EEEEE")
+        
+        // 5 - set up the day name labels with correct days
+        for i in 0...maxDayIndex {
+            if let date = calendar.date(byAdding: .day, value: -i, to: today),
+                let label = stackView.arrangedSubviews[maxDayIndex - i] as? UILabel {
+                label.text = formatter.string(from: date)
+            }
+        }
+    }
+
     
 }
 
